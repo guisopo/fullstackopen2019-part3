57f95afbd5b4c5487ca647eb6cbb9898184635f7
guisopo <solerguillermo@hotmail.com>
2019-07-10 13:40:19 +0200

    Add production build of the frontend

---
+++ build/precache-manifest.15f4c01e4030c310801406a418025055.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9009658294b49b2b075fb98c9be8ed16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edb70b9760340e495c07",
+    "url": "/static/js/2.42c16a5d.chunk.js"
+  },
+  {
+    "revision": "d279379e33940616b875",
+    "url": "/static/js/main.83f53581.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
