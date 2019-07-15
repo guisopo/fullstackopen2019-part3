ac63cd6a0082cfd4447f8ce028865424d141cc8e
guisopo <solerguillermo@hotmail.com>
2019-07-15 21:34:53 +0200

    Clean up

--- index.js
+++ index.js
@@ -117,13 +117,9 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.log(error)
   if (error.name === 'CastError' && error.kind === 'ObjectId') {
     return response.status(400).send({ error: 'Malformatted ID' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
   }
-
   next(error)
 }
 
